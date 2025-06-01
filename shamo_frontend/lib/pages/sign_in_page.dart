51af94e054241cec6ab08b3060a2e77d98484280
Aidil Baihaqi <aidilmusirjun@gmail.com>
2025-06-02 06:31:48 +0700

    adding theme, main page

---
+++ shamo_frontend/lib/pages/sign_in_page.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class SignInPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Center(child: Text("Sign in page")));
+  }
+}
