9389daa25e7fe9adeee1f81eb7749e42147d7d42
Aidil Baihaqi <aidilmusirjun@gmail.com>
2025-06-01 23:11:38 +0700

    controller: user fetch api

--- shamo-backend/routes/api.php
+++ shamo-backend/routes/api.php
@@ -4,9 +4,10 @@ use App\Http\Controllers\API\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
 
 Route::post('register', [UserController::class, 'register']);
 Route::post('login', [UserController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('user', [UserController::class, 'fetch']);
+});
