98830893400f96151c9e0b76962507d456f3acc1
SyawalN <syawalnopyandi1103@gmail.com>
2025-06-09 18:50:03 +0700

    Fix tailwinds.config.js not loading

--- shamo_backend/tailwind.config.js
+++ shamo_backend/tailwind.config.js
@@ -13,9 +13,11 @@ export default {
 
     theme: {
         extend: {
-            fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
-            },
+            screens: {
+                fontFamily: {
+                    sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+                },
+            }
         },
     },
 
