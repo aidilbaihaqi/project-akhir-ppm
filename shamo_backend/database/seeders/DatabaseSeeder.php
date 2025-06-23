2a9b72d4f41dbf0fcd32fc41cfcfebb932f9d031
SyawalN <syawalnopyandi1103@gmail.com>
2025-06-23 22:08:17 +0700

    Added user data seeder to DatabaseSeeder.php

--- shamo_backend/database/seeders/DatabaseSeeder.php
+++ shamo_backend/database/seeders/DatabaseSeeder.php
@@ -3,21 +3,25 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
     /**
      * Seed the application's database.
+     *
+     * @return void
      */
-    public function run(): void
+    public function run()
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        User::create([
+            "name" => "Administrator",
+            "email" => "admin@example.com",
+            "username" => "Admin Kece",
+            "phone" => "08123456789",
+            "roles" => "ADMIN",
+            "password" => Hash::make("admin123")
         ]);
     }
 }
